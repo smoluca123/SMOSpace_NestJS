601d40a6ed9ddd6656b09a2e7ba5ba2ca0a8791f
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-09-26 15:09:11 +0700

    add resource auth

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
