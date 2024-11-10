fdefd691d27d76679f86241f36dde036e5f7ccd0
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-11-10 23:56:19 +0700

    add route /active/:userId, /active/send-verification-email/:userId

--- src/global/functions.global.ts
+++ src/global/functions.global.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 import { BadRequestException } from '@nestjs/common';
 
 export const handleDefaultError = (error: any) => {
@@ -9,4 +11,8 @@ export const handleDefaultError = (error: any) => {
 export function sanitizeFileName(fileName) {
   // Loại bỏ các ký tự không hợp lệ: chỉ giữ lại chữ cái, số, dấu gạch ngang và dấu gạch dưới
   return fileName.replace(/[^a-zA-Z0-9-_\.]/g, '').replace(/[\s]/g, '_'); // Thay thế các khoảng trắng (space) bằng dấu gạch dưới (_)
+}
+
+export function generateSecureVerificationCode() {
+  return crypto.randomBytes(3).toString('hex').toUpperCase();
 }
--- src/resources/user/user.module.ts
+++ src/resources/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { SupabaseService } from 'src/supabase/supabase.service';
+import { EmailModule } from 'src/resources/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [UserController],
   providers: [UserService, SupabaseService],
 })
